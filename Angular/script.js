20a35a369677f5723472eea64e68ce65d6fe12f9
rajavhale <raj.avhale.sid@gmail.com>
2016-04-07 19:01:56 +0530

    angular files

---
+++ Angular/script.js
@@ -0,0 +1,8 @@
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+    $scope.message="angular";
+    $scope.firtName="pallavi";
+    $scope.lastName="gaikwad";
+    $scope.firstNo=5;
+    $scope.secondNo=7;
+});
